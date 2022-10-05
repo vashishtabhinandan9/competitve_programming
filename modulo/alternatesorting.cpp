e8fbf2ab507c6709d1bb57320cef092a103a058f
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-10-05 13:04:44 +0530

    nothing

---
+++ modulo/alternatesorting.cpp
@@ -0,0 +1,17 @@
+/**
+ * question:
+ * Given an array of integers, print the array in such a way that
+ *  the first element is first maximum and second element is first minimum and so on.
+*/
+//see notes in your register
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+//1st method
+
+
+
+int main(){
+  
+    return 0;
+}
---
+++ sorting/mergesort/mergesort(inplace,outplace).cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+//1st method
+
+int main(){
+   int n;
+       cin>>n;
+       vector<int> arr(n,0);
+       for(int i=0;i<n;i++) cin>>arr[i];
+
+       mergeSort(arr,0,n-1);
+    return 0;
+}
+
+void inPlaceMerge(vector<int> &nums, int low, int mid, int high) {
+	if (low >= high) return;
+	int l = low, r = mid + 1, size = high - low + 1;
+	while (l <= mid and r <= high) {
+		if (nums[l] <= nums[r]) l++;
+		else {
+			int val = nums[r];
+			for (int k = r++; k > l; k--)
+				nums[k] = nums[k - 1];
+			nums[l++] = val;
+			mid++;
+		}
+	}
+}
+
+void outPlaceMerge(vector<int> &nums, int low, int mid, int high) {
+	if (low >= high) return;
+	int l = low, r = mid + 1, k = 0, size = high - low + 1;
+	vector<int> sorted(size, 0);
+	while (l <= mid and r <= high)
+		sorted[k++] = nums[l] < nums[r] ? nums[l++] : nums[r++];
+	while (l <= mid) 
+		sorted[k++] = nums[l++];
+	while (r <= high) 
+		sorted[k++] = nums[r++];
+	for (k = 0; k < size; k++)
+		nums[k + low] = sorted[k];
+}
+
+void mergeSort(vector<int> &nums, int low, int high) {
+	if (low >= high) return;
+	int mid = (high - low) / 2 + low;
+	mergeSort(nums, low, mid);
+	mergeSort(nums, mid + 1, high);
+	inPlaceMerge(nums, low, mid, high);
+    outPlaceMerge(nums, low, mid, high);
+}
